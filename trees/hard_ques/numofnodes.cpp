3a5f0bb04b790e793054d75007824f6bbb14f0d7
aarush2109 <aarushm2109@gmail.com>
2026-01-16 00:02:01 +0530

    numofnodes question done

---
+++ trees/hard_ques/numofnodes.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int count = 0;
+
+    int countNodes(TreeNode* root) {
+        if (root == NULL) return count;
+
+        count++;
+        countNodes(root->left);
+        countNodes(root->right);
+
+        return count;
+    }
+};
+
+int main() {
+    /*
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+    */
+
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+
+    Solution sol;
+    int total = sol.countNodes(root);
+
+    cout << "Total number of nodes: " << total << endl;
+
+    return 0;
+}
